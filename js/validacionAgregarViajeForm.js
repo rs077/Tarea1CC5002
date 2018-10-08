c1768d7f87d9eca62a56d7afb00d8e8250cc93b5
rs077 <rodrigo.soria@ing.uchile.cl>
2018-10-08 14:16:39 -0300

    validaciones region y comunas agregarViajes listas

--- js/validacionAgregarViajeForm.js
+++ js/validacionAgregarViajeForm.js
@@ -4,7 +4,7 @@ function validateEmail(email)
     {
         return (true)
     }
-    alert("You have entered an invalid email address!")
+    alert("email invalido")
     return (false)
 }
 function validatePhone(phone) {
@@ -12,15 +12,38 @@ function validatePhone(phone) {
     var stripped = phone.replace(/[\(\)\.\-\ ]/g, '');
 
     if (stripped == "") {
-        error = "You didn't enter a phone number.";
+        error = "Ingrese un celular.";
         alert(error)
     } else if (isNaN(parseInt(stripped))) {
         phone = "";
-        error = "The phone number contains illegal characters.";
+        error = "El numero de caracteres no permitidos.";
         alert(error)
-    } else if (!(stripped.length == 10)) {
+    } else if (!(stripped.length == 12)) {
         phone = "";
-        error = "The phone number is the wrong length. Make sure you included an area code.\n";
+        error = "El celular tiene un largo incorrecto.";
+        alert(error)
+    }
+    return (false)
+}
+function validateRegionComunaOrigen(region,comuna) {
+    var error = "";
+    if (region == "sin-region" || comuna == "sin-region" ) {
+        error = "Ingrese region de origen.";
+        alert(error)
+    }
+    else if (comuna == "sin-comuna" ) {
+        error = "Ingrese comuna de origen.";
+        alert(error)
+    }
+}
+function validateRegionComunaDestino(region,comuna) {
+    var error = "";
+    if (region == "sin-region" || comuna == "sin-region" ) {
+        error = "Ingrese region de destino.";
+        alert(error)
+    }
+    else if (comuna == "sin-comuna" ) {
+        error = "Ingrese comuna de destino.";
         alert(error)
     }
 }
@@ -28,18 +51,20 @@ function validateForm() {
     var regionOrigen = document.forms["formAgregarViaje"]["region-origen"].value;
     var comunaOrigen = document.forms["formAgregarViaje"]["comuna-origen"].value;
     var regionDestino = document.forms["formAgregarViaje"]["region-destino"].value;
-    var regionDestino = document.forms["formAgregarViaje"]["comuna-destino"].value;
+    var comunaDestino = document.forms["formAgregarViaje"]["comuna-destino"].value;
     var fechaViaje = document.forms["formAgregarViaje"]["fecha-viaje"].value;
     var espacioDisponible = document.forms["formAgregarViaje"]["espacio-disponible"].value;
     var kilosDisponibles = document.forms["formAgregarViaje"]["kilos-disponibles"].value;
     var email = document.forms["formAgregarViaje"]["email"].value;
     var celular = document.forms["formAgregarViaje"]["celular"].value;
-    if (validateEmail(email) && validatePhone(celular)){
+    window.alert(regionOrigen+" "+comunaOrigen+" "+regionDestino+" "+comunaDestino+" fecha:"
+        +fechaViaje+". "+espacioDisponible+" "+kilosDisponibles);
+    if (validateEmail(email) && validatePhone(celular) && validateRegionComunaOrigen(regionOrigen,comunaOrigen)
+    && validateRegionComunaDestino(regionDestino, comunaDestino)){
+        alert("Viaje agregado.")
         return true
     }
-    else {
-        return false
-    }
+    return false
 }
 
 
